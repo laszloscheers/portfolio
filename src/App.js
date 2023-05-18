f368f3a132f200a20b274315b8d7c3b394407a6a
laszloscheers <laszloscheers.gmail.com>
2023-05-18 23:42:25 +0200

    routes fix attempt

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import AppNavbar from './components/AppNavbar/AppNavbar';
 import Footer from './components/Footer/Footer';
 import Home from './pages/Home/Home';
